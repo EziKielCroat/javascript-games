52a2b3d90d32dd2c139ce86fecd620a4f42f2214
EziKielCroat <89482125+EziKielCroat@users.noreply.github.com>
2022-08-31 15:08:05 +0200

    original upload
    multiplayer still doesn't work everything else does

---
+++ criss-cross-public/multiplayer/client.js
@@ -0,0 +1,90 @@
+
+
+// Bitno
+
+
+document.getElementById("request").addEventListener("click", function(){
+  let element = document.getElementById("buttons");
+  while(element.firstChild){
+    element.removeChild(element.firstChild);
+  }
+  document.getElementById("room-info").style.display = "block";
+});
+
+document.getElementById("join").addEventListener("click", function(){
+  let element = document.getElementById("buttons");
+  while(element.firstChild){
+    element.removeChild(element.firstChild);
+  }
+  document.getElementById("room-join").style.display = "block";
+});
+
+function gameLogic(players) {
+  let elements = document.getElementsByClassName("block");
+
+  Array.prototype.forEach.call(elements, function(el) {
+      document.getElementById(el.id).addEventListener("click", function() {
+        sendServer("move" + players, el.id)
+      });
+  });
+}
+
+
+// Helper functions
+
+function startGame() {
+  document.getElementById("room-info").style.display = "none";
+  document.getElementById("room-join").style.display = "none";
+
+  document.getElementById("container").style.display = "none";
+  document.getElementById("container2").style.display = "flex";
+}
+
+function sendServer(type, additional) {
+
+  const socket = io();
+
+  socket.on("start_game", function() {
+    startGame();
+    socket.emit("game_initialized");
+  });
+
+  socket.on("turnO", function() {
+    let player = "O";
+    gameLogic(player);
+  })
+
+  socket.on("turnX", function() {
+    let player = "X";
+    gameLogic(player);
+  })
+
+  if (type == "formMake") {
+
+    document.getElementById("wait").style.display = "block";
+    document.getElementById("room-info").style.display = "none";
+
+    let userName = document.getElementById("name").value
+    let userRoom = document.getElementById("room").value
+    let p = document.getElementById("pass").value
+
+    let dataSend = [userName, userRoom, p];
+
+    socket.emit("register_room", dataSend);
+
+  } else if (type == "formJoin") {
+
+    document.getElementById("room-join").style.display = "none";
+
+    let userName = document.getElementById("nameJoin").value
+    let userRoom = document.getElementById("roomJoin").value
+    let p = document.getElementById("passJoin").value
+
+    let dataSend = [userName, userRoom, p];
+
+    socket.emit("login_room", dataSend);
+  } else if( type =="moveO") {
+    socket.emit("moveO", additional);
+  }
+}
+
